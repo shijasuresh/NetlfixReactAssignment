9bfaf5eea56168e2b0e0eb3979894a3199f95b9b
shijasuresh <shija.suresh89@gmail.com>
2024-09-23 12:56:40 +1000

    Completed Most Popular Feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
+import MostPopular from "./pages/MostPopular";
 
 function App() {
   return(
     <>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/mostpopular" element={<MostPopular />}/>
         <Route path="/details/:slug" element={<Details />}/>
       </Routes>
     </>
